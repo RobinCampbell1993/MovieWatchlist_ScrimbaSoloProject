f174900a746658dbafbc07296fe600bcf4893451
Robin Campbell <74200004+RobinCampbell1993@users.noreply.github.com>
2022-09-16 19:30:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,109 @@
+const searchBtn = document.getElementById('search-btn')
+const searchInput = document.getElementById('search-input')
+const moviesDiv = document.getElementById('movies')
+const placeHolder = document.getElementsByClassName("placeholder-image-container")
+
+let savedMovies = JSON.parse(localStorage.getItem("savedMovies"))
+let movieDataHtmlArray = []
+
+searchBtn.addEventListener('click', searchAPI)
+searchInput.addEventListener('keypress', e => {
+    if (e.key === "Enter") {
+        searchAPI()
+    }
+})
+
+function searchAPI() {
+    placeHolder[0].style.display = "none"
+    const searchTerm = searchInput.value
+    movieDataHtmlArray = []
+
+    fetch(`http://www.omdbapi.com/?apikey=9c43412e&s=${searchTerm}&type=movie`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Network error. Please try again later.")
+            } else {
+                return res.json()
+            }
+        })
+        .then(async data => {
+            try {
+                moviesDiv.innerHTML = ``
+                placeHolder[0].style.display = "flex"
+                placeHolder[0].innerHTML = `
+                        <div class="loader"></div>
+                        <p>Fetching movies, please wait...</p>
+                    `
+                await fetchMovieData(data)
+                placeHolder[0].style.display = "none"
+                moviesDiv.innerHTML = movieDataHtmlArray.join("")
+            } catch (err) {
+                placeHolder[0].style.display = "flex"
+                placeHolder[0].innerHTML = `<p>${data.Error} Please search again.</p>`
+            }
+
+            const addToWatchlistBtn = document.getElementsByClassName("add-to-watchlist-container")
+
+            for (btn of addToWatchlistBtn) {
+                btn.addEventListener("click", e => {
+                    const targetIdClass = e.target.parentNode.getAttribute("class")
+                    const targetId = targetIdClass.replace("add-to-watchlist-container ", "")
+
+                    console.log(targetId)
+                    if (savedMovies !== null && savedMovies.includes(targetId)) {
+                        alert(`You have already added this movie to your watchlist.`);
+                    } else if (savedMovies === null) {
+                        savedMovies = []
+                        savedMovies.unshift(targetId)
+                        localStorage.setItem("savedMovies", JSON.stringify(savedMovies))
+                        alert(`This movie has been added to your watchlist!`);
+                    } else {
+                        savedMovies.unshift(targetId)
+                        localStorage.setItem("savedMovies", JSON.stringify(savedMovies))
+                        alert(`This movie has been added to your watchlist!`);
+                    }
+                    console.log(savedMovies)
+                    // console.log(localStorage)
+                })
+            }
+        })
+        .catch(err => console.log(err))
+}
+
+async function fetchMovieData(data) {
+    for (movie of data.Search) {
+        const response = await fetch(`http://www.omdbapi.com/?apikey=9c43412e&i=${movie.imdbID}&type=movie`)
+        const data = await response.json()
+
+        console.log(data)
+        const { Poster, Title, imdbRating, Runtime, Genre, Plot, imdbID } = data
+
+        const html = `
+        <div class="movie-container" id="${imdbID}">
+                <img class="movie-image" src="${Poster === "N/A" ? "/images/film-logo.png" : Poster}" alt="${Title} poster">
+                <div class="movie-item-info">
+                    <div class="movie-container-title">
+                        <h3 class="movie-title"><a target="_blank" href='https://www.imdb.com/title/${imdbID}'>${Title}</a></h3>
+                        <img src="/images/star-image.png">
+                        <p class="movie-rating">${imdbRating}</p>
+                    </div>
+    
+                    <div class="movie-container-info">
+                        <p class="move-duration">${Runtime}</p>
+                        <p class="movie-genre">${Genre}</p>
+                        <div class="add-to-watchlist-container ${imdbID}" id="add-to-watchlist-btn">
+                            <img class="add-to-watchlist-img" src="/images/plus-btn-image.png">
+                            <p>Watchlist</p>
+                        </div>
+                    </div>
+    
+                    <div class="description" id="description">
+                        <p>${Plot}</p>
+                    </div>
+                </div>
+            </div>
+        `
+
+        movieDataHtmlArray.push(html)
+    }
+}
---
+++ watchlist.js
@@ -0,0 +1,71 @@
+const moviesDiv = document.getElementById("movies")
+const placeHolder = document.getElementsByClassName("placeholder-image-container")
+
+const displayMovies = []
+let savedMovies = JSON.parse(localStorage.getItem("savedMovies"))
+
+async function getSavedMovies() {
+    for (movie of savedMovies) {
+        const response = await fetch(`http://www.omdbapi.com/?apikey=9c43412e&i=${movie}&type=movie`)
+        const data = await response.json()
+
+        console.log(data)
+        renderMovies(data) 
+
+        const removeBtn = document.getElementsByClassName("add-to-watchlist-container")
+
+        for (btn of removeBtn) {
+            btn.addEventListener("click", e => {
+                const targetIdClass = e.target.parentNode.getAttribute("class")
+                console.log(targetIdClass)
+                const targetId = targetIdClass.replace("add-to-watchlist-container ", "")
+                updatedMovieList = savedMovies.filter(movie => {
+                    return movie != targetId
+                })
+
+                localStorage.setItem("savedMovies", JSON.stringify(updatedMovieList))
+                location.reload()
+                alert(`This movie has been removed from your watchlist.`)
+            })
+        }
+    }
+}
+
+function renderMovies(data) {
+    const { Poster, Title, imdbRating, Runtime, Genre, Plot, imdbID } = data
+    const html = `
+    <div class="movie-container" id="${imdbID}">
+            <img class="movie-image" src="${Poster}" alt="${Title} image">
+            <div class="movie-item-info">
+                <div class="movie-container-title">
+                    <h3 class="movie-title"><a target="_blank" href='https://www.imdb.com/title/${imdbID}'>${Title}</a></h3>
+                    <img src="/images/star-image.png">
+                    <p class="movie-rating">${imdbRating}</p>
+                </div>
+
+                <div class="movie-container-info">
+                    <p class="move-duration">${Runtime}</p>
+                    <p class="movie-genre">${Genre}</p>
+                    <div class="add-to-watchlist-container ${imdbID}" id="remove-from-watchlist-btn">
+                        <img class="add-to-watchlist-img" src="/images/minus-btn-image.png">
+                        <p>Remove</p>
+                    </div>
+                </div>
+
+                <div class="description" id="description">
+                    <p>${Plot}</p>
+                </div>
+            </div>
+        </div>
+    `
+    displayMovies.push(html)
+    moviesDiv.innerHTML = displayMovies.join("")
+}
+
+console.log(savedMovies)
+if (savedMovies.length !== 0) {
+    placeHolder[0].style.display = "none"
+    getSavedMovies()
+} else {
+    placeHolder[0].style.display = "flex"
+}
